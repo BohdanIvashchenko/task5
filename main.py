d8ac31ae82d2506e0c430820e730ae74737eb604
Bohdan <ivasch@knu.ua>
2025-02-20 01:49:49 +0200

    powerof2

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from utils import *
 
 print(factorial(5))
+<<<<<<< Updated upstream
+=======
+print(powerof5(1435))
+print(powerof2(128))
+>>>>>>> Stashed changes
--- utils.py
+++ utils.py
@@ -8,4 +8,18 @@ def prime(n):
 	for i in range(2, n):
 			if n % i == 0:
 				return False
+<<<<<<< Updated upstream
 	return True
+=======
+	return True
+
+def powerof5(n):
+	while n > 1:
+		n /= 5
+	return n == 1
+
+def powerof2(n):
+	while n > 1:
+		n /= 2
+	return n == 1
+>>>>>>> Stashed changes
