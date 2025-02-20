6d3ba64d8acd51ba6b764aba4c22f13bfb7ccf60
Bohdan <ivasch@knu.ua>
2025-02-20 02:15:11 +0200

    conflict resolution

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from utils import *
 
 print(factorial(5))
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 print(powerof5(1435))
 print(powerof2(128))
->>>>>>> Stashed changes
-=======
-print(powerof5(1435))
->>>>>>> dev5
